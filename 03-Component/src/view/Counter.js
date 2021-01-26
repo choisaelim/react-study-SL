6fea720a68a27d7d7c08467a4ad5408d899c572e
choisaelim <choisaelim@gmail.com>
2021-01-27 00:16:19 +0900

    event 처리 끝

--- 03-Component/src/view/Counter.js
+++ 03-Component/src/view/Counter.js
@@ -3,25 +3,28 @@ import React, { Component } from 'react';
 //rcc
 class Counter extends Component {
     state = {
-        number: 0,
+        number : 0,
         fixedNumber: 0
     };
-
-    buttonClick(props){
-        // var num = type === 'plus' ? 1 : -1;
-        this.setState((prevState, props) => ({
-            // number: prevState.number + 1
-        }));
-    }
-
+    
     render() {
         const {number, fixedNumber} = this.state;
+        const numberChange = (type) => {
+            var num = type === 'plus' ? 1 : -1;
+            this.setState((prevState, props) => ({
+                number: prevState.number + num
+            }),
+            () => {
+                console.log('setState called');                
+            }
+            );
+        }
         return (
             <div>
                 <h3>{number}</h3> 
                 <h3>{fixedNumber}</h3>
-                <button onClick={this.buttonClick}>+1</button>
-                <button>-1</button>
+                <button onClick = {() => numberChange('plus')}>+ 1</button>
+                <button onClick = {() => numberChange('minus')}>-1</button>
             </div>
         );
     }
