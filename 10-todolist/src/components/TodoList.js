77c1bbd8eb147dab4fc7857e39121234f58db45f
choisaelim <choisaelim@gmail.com>
2021-03-03 22:33:41 +0900

    react-virtualizer 활용하여 rowRenderer 사용

--- 10-todolist/src/components/TodoList.js
+++ 10-todolist/src/components/TodoList.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import TodoListItem from './TodoListItem';
 import '../scss/TodoTemplate.scss';
+import {List} from 'react-virtualized';
 
 const TodoList = ({todos, onRemove, onToggle}) => {
+    const rowRenderer = useCallback(
+        ({index, key, style}) => {
+            const todo = todos[index];
+            return (
+                <TodoListItem
+                todo = {todo}
+                key = {key}
+                style = {style}
+                onRemove = {onRemove}
+                onToggle = {onToggle}
+                />
+            );
+        },
+        [onRemove, onToggle, todos],
+    );
     return (
-        <div className = 'TodoList'>
-            {todos.map((todo) => (
-                <TodoListItem todo = {todo} key = {todo.id} onRemove = {onRemove} onToggle = {onToggle}/>
-            ))}
-        </div>
-    )
+        // <div className = 'TodoList'>
+        //     {todos.map((todo) => (
+        //         <TodoListItem todo = {todo} key = {todo.id} onRemove = {onRemove} onToggle = {onToggle}/>
+        //     ))}
+        // </div>
+        <List className = 'TodoList' 
+            width = {512}
+            height = {513}
+            rowCount = {todos.length}
+            rowHeight = {57}
+            rowRenderer = {rowRenderer}
+            list = {todos}
+            style = {{outline: 'none'}}
+        />
+    );
 }
 
 export default React.memo(TodoList);
