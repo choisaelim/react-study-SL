d8cfd8540aa754c54411df03431c3e81af0bdeb5
choisaelim <75531480+choisaelim@users.noreply.github.com>
2021-04-07 23:27:47 +0900

    newsList 틀 만들기

--- 14-news-viewer/src/App.js
+++ 14-news-viewer/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
 	const [data, setData] = useState(null);
@@ -13,12 +14,13 @@ const App = () => {
 		}
 	};
 	return (
-		<div>
-			<div>
-				<button onClick={onClick}>불러오기</button>
-			</div>
-			{data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-		</div>
+		// <div>
+		// 	<div>
+		// 		<button onClick={onClick}>불러오기</button>
+		// 	</div>
+		// 	{data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+		// </div>
+		<NewsList />
 	);
 };
 
