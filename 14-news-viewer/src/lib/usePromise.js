fbe420c2a9b526d1992c9d285ecb207b1271132d
choisaelim <75531480+choisaelim@users.noreply.github.com>
2021-04-11 22:43:06 +0900

    useEffect로 Promise 훅 만들기

---
+++ 14-news-viewer/src/lib/usePromise.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+//rfc
+export default function usePromise(promiseCreator, deps) {
+	//대기 중/완료/실패 상태 관리
+	const [loading, setLoading] = useState(false);
+	const [resolved, setResolved] = useState(null);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const process = async () => {
+			setLoading(true);
+			try {
+				const resolved = await promiseCreator();
+				setResolved(resolved);
+			} catch (e) {
+				setError(e);
+			}
+			setLoading(false);
+		};
+		process();
+	}, deps);
+	return [loading, resolved, error];
+}
